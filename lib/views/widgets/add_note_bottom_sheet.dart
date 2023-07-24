33faa5b41bed34b4cbf1aa2c198e2fbae8aa9dcd
Mina Adly <mina2dly@gmial.com>
2023-07-24 20:50:46 +0300

    refactor multi bloc provider

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -9,22 +9,25 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if (state is AddNoteSuccessState) {
-            Navigator.pop(context);
-          } else if (state is AddNoteFailureState) {
-            debugPrint('Failed ${state.errMessage}');
-          }
-        },
-        builder: (context, state) {
-          return ModalProgressHUD(
-            inAsyncCall: state is AddNoteLoadingState ? true : false,
-            child: const SingleChildScrollView(child: const AddNoteForm()),
-          );
-        },
+    return BlocProvider(
+      create: (BuildContext context) => AddNoteCubit(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccessState) {
+              Navigator.pop(context);
+            } else if (state is AddNoteFailureState) {
+              debugPrint('Failed ${state.errMessage}');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoadingState ? true : false,
+              child: const SingleChildScrollView(child: AddNoteForm()),
+            );
+          },
+        ),
       ),
     );
   }
