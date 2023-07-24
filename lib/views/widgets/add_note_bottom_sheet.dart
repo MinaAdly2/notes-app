77c09161f32c93fa40f82606efd40de2424f853b
Mina Adly <mina2dly@gmial.com>
2023-07-24 20:46:29 +0300

    trigger add note cubit

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -11,22 +11,20 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if (state is AddNoteSuccessState) {
-              Navigator.pop(context);
-            } else if (state is AddNoteFailureState) {
-              debugPrint('Failed ${state.errMessage}');
-            }
-          },
-          builder: (context, state) {
-            return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoadingState ? true : false,
-              child: const AddNoteForm(),
-            );
-          },
-        ),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteSuccessState) {
+            Navigator.pop(context);
+          } else if (state is AddNoteFailureState) {
+            debugPrint('Failed ${state.errMessage}');
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoadingState ? true : false,
+            child: const SingleChildScrollView(child: const AddNoteForm()),
+          );
+        },
       ),
     );
   }
