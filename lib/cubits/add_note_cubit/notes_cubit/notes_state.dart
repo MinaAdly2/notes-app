5c05b81af28cef7e8b8d797736b3e8d6b48b9874
Mina Adly <mina2dly@gmial.com>
2023-07-25 16:54:41 +0300

    create notes state

---
+++ lib/cubits/add_note_cubit/notes_cubit/notes_state.dart
@@ -0,0 +1,20 @@
+part of 'notes_cubit.dart';
+
+@immutable
+abstract class NotesState {}
+
+class NotesInitial extends NotesState {}
+
+class NotesLoadingState extends NotesState {}
+
+class NotesSuccessState extends NotesState {
+  final List<NoteModel> notes;
+
+  NotesSuccessState({required this.notes});
+}
+
+class NotesFailureState extends NotesState {
+  final String errMessage;
+
+  NotesFailureState({required this.errMessage});
+}
